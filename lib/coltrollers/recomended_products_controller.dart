b5cb62bb2c4f43bf5fdb0d3ffe7d9d80c9de58f8
NATNAEL BIRHANU <negusnati@gmail.com>
2023-03-18 13:02:00 -0700

    recomended repo

---
+++ lib/coltrollers/recomended_products_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+
+import '../data/api/repository/popular_product_repo.dart';
+import '../data/api/repository/recomended_product_repo.dart';
+import '../models/popular_model.dart';
+
+class RecomendedProductController extends GetxController {
+  final RecomendedPoductRepo recomendedPoductRepo;
+  RecomendedProductController({required this.recomendedPoductRepo});
+
+  List<dynamic> _recomendedProductList = [];
+  List<dynamic> get recomendedProductList => _recomendedProductList;
+  bool _isLoaded = false;
+  bool get isLoaded => _isLoaded;
+
+  Future<void> getPopularProductList() async {
+    Response response = await recomendedPoductRepo.getRecomendedProductList();
+    // if (response.statusCode == 200) {
+    //   _popularProductList = [];
+    //   // create a model to accept the data from json
+    //   // _popularProductList.addAll();
+    //   update(); //is like setState for Getx
+    // } else {}
+
+    if (response.statusCode == 200) {
+      print("got products");
+      _recomendedProductList = [];
+      _recomendedProductList.addAll(Product.fromJson(response.body).products);
+      //  Map rawData = jsonDecode(response.body);
+      // _popularProductList.addAll(Product.fromJson(rawData).products);
+      //print(_popularProductList);
+      _isLoaded = true;
+      update();
+    } else {
+      print("No products");
+    }
+  }
+}
---
+++ lib/data/api/repository/recomended_product_repo.dart
@@ -0,0 +1,15 @@
+import 'package:food_delivery_app/data/api/api_client.dart';
+import 'package:get/get.dart';
+
+import '../../../utills/app_constants.dart';
+
+class RecomendedPoductRepo extends GetxService {
+  final ApiClient apiClient;
+
+ RecomendedPoductRepo({required this.apiClient});
+
+  Future<Response> getRecomendedProductList() async {
+    //TODO: End point 
+    return await apiClient.getData(AppConstants.POPULAR_PRODUCT_URI);
+  }
+}
