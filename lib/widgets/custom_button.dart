1b95fb53fb2918449b5c88afdff9dcf6dd7db27f
NATNAEL BIRHANU <negusnati@gmail.com>
2023-05-11 00:34:27 -0700

    custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,36 +1,81 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'package:food_delivery_app/utills/dimensions.dart';
 
 class CustomButton extends StatelessWidget {
- final VoidCallback? onPressed;
- final String buttonText;
- final bool transparent;
- final EdgeInsets? margin;
- final double? height;
- final double? widht;
- final double? fontSize;
- final double radius;
- final IconData? icon;
- 
- 
-  CustomButton({
+  final VoidCallback? onPressed;
+  final String buttonText;
+  final bool transparent;
+  final EdgeInsets? margin;
+  final double? height;
+  final double? width;
+  final double? fontSize;
+  final double radius;
+  final IconData? icon;
+
+  const CustomButton({
     Key? key,
     this.onPressed,
     required this.buttonText,
-    required this.transparent,
+    this.transparent = false,
     this.margin,
     this.height,
-    this.widht,
+    this.width,
     this.fontSize,
-    required this.radius,
+    this.radius = 5,
     this.icon,
   }) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final ButtonStyle _flatButton = TextButton.styleFrom(
+      backgroundColor: onPressed == null
+          ? Theme.of(context).disabledColor
+          : transparent
+              ? Colors.transparent
+              : Theme.of(context).primaryColor,
+      minimumSize: Size(width != null ? width! : Dimensions.screenWidth,
+          height != null ? height! : 50),
+      padding: EdgeInsets.zero,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(radius),
+      ),
+    );
+    return Center(
+      child: SizedBox(
+        width: width ?? Dimensions.screenWidth,
+        height: height ?? 50,
+        child: TextButton(
+          onPressed: () {},
+          style: _flatButton,
+          child: Row(children: [
+            icon != null
+                ? Padding(
+                    padding: EdgeInsets.only(
+                      right: Dimensions.Width5,
+                    ),
+                    child: Icon(
+                      icon,
+                      color: transparent
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).cardColor,
+                    ),
+                  )
+                : SizedBox(),
+            Text(
+              buttonText,
+              style: TextStyle(
+                fontSize: fontSize ?? Dimensions.fontSize16,
+                color: transparent
+                    ? Theme.of(context).primaryColor
+                    : Theme.of(context).cardColor,
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
