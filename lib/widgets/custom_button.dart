f43149ee22d67e51b58bc6ba8b0c890acde1a9ce
NATNAEL BIRHANU <negusnati@gmail.com>
2023-05-10 23:37:56 -0700

    some bug fix

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
+
 import 'package:food_delivery_app/utills/dimensions.dart';
 
 class CustomButton extends StatelessWidget {
@@ -12,9 +13,19 @@ class CustomButton extends StatelessWidget {
  final double? fontSize;
  final double radius;
  final IconData? icon;
-
+ 
+ 
   CustomButton({
     Key? key,
+    this.onPressed,
+    required this.buttonText,
+    required this.transparent,
+    this.margin,
+    this.height,
+    this.widht,
+    this.fontSize,
+    required this.radius,
+    this.icon,
   }) : super(key: key);
 
   // This widget is the root of your application.
